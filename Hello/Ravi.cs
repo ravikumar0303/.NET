d68859398efdfcaa696ccc77fa55a3a7d1fa703c
ravi <raravikumar432@gmail.com>
2023-07-11 13:51:58 +0530

    hello and using constructor

---
+++ Hello/Ravi.cs
@@ -0,0 +1,37 @@
+namespace r1;
+
+class Ravi{
+	private String name;
+	private int rollNum;
+	
+	public Ravi(){
+		this.name = "asd";
+		this.rollNum = 3;
+	}
+	
+	public Ravi(String name, int rollNum){
+		this.name = name;
+		this.rollNum = rollNum;
+	}
+	
+	public String GetName(){
+		return this.name;
+	}
+	
+	public void SetName(String name){
+		this.name = name;
+	}
+	
+	public int GetRollNum(){
+		return this.rollNum;
+	}
+	
+	public void SetRollNum(int rollNum){
+		this.rollNum = rollNum;
+	}
+	
+	public override string ToString(){
+        return base.ToString() + " "+
+          this.rollNum + " "+ this.name;
+    }
+}
